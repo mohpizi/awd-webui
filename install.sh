3da934d68dd9dd4da53fff736dc0a2c019b1f1b8
mohpizi <mohpiziae@gmail.com>
2026-02-15 03:56:32 +0700

    Update biasa: 2026-02-15 03:56:31

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+DIR_MODUL="/data/adb/modules/php8-webserver/"
+cd "$DIR_MODUL" || { echo "❌ Folder $DIR_MODUL tidak ditemukan!"; exit 1; }
+if [ ! -f "module.prop" ]; then
+    echo "❌ File module.prop tidak ditemukan di folder ini!"
+    exit 1
+fi
+VERSION_CODE=$(date +'%Y%m%d')
+echo "📦 Memperbarui versi modul Magisk..."
+echo "--- Info Saat Ini ---"
+grep "^version=" module.prop
+grep "^versionCode=" module.prop
+echo "---------------------"
+version=3.3.3
+
+if [ -z "$NEW_VERSION" ]; then
+    echo "❌ Versi tidak boleh kosong! Dibatalkan."
+    exit 1
+fi
+sed -i "s/^version=.*/version=${version}/g" module.prop
+sed -i "s/^versionCode=.*/versionCode=${VERSION_CODE}/g" module.prop
+
+echo ""
+echo "✅ module.prop berhasil diperbarui!"
+echo "--- Info Terbaru ---"
+grep "^version=" module.prop
+grep "^versionCode=" module.prop
+echo "--------------------"
